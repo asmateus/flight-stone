b65fc0672350a4c15869539dbaf695ff42c7bf9e
asmateus <mateus@uninorte.edu.co>
2017-07-25 20:32:12 -0500

    Created serializer module

--- filght-stone/director/serial/serializer.py
+++ filght-stone/director/serial/serializer.py
@@ -1,4 +1,13 @@
 '''
 This is a two way serialization system. It buffers in both manager's data and orchesta information
-for further stages of processing.
+for further stages of processing. Simply explained Serializer takes the input from a connected
+device and fowards its information to any requester.
 '''
+
+
+class Buffer():
+    def __init__(self):
+        pass
+
+    def __call__(self):
+        pass
