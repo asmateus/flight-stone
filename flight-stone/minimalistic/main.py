e325f7d9601b354a07beeef93decb950c9763abf
Jorgee1 <jorgepacheco@uninorte.edu.co>
2017-09-12 21:51:53 -0500

    Arreglo minimalista

--- flight-stone/minimalistic/main.py
+++ flight-stone/minimalistic/main.py
@@ -2,8 +2,8 @@ import serial
 import time
 
 #python -m serial.tools.list_ports
-
 ser = serial.Serial("/dev/ttyUSB0")
+ser.baudrate = 10417
+print("Init")
 while(True):
     print(ser.readline())
-    time.sleep(2)
